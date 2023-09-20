9a8d74d1406b0bfa36ed98e549c12014c60b7061
Quang Ho <mxrshmalloww@gmail.com>
2023-09-19 20:41:18 -0400

    added a few things

---
+++ AppGui/AppGui/LoginMenu.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AppGui
+{
+    partial class LoginMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Button();
+            this.accountCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(267, 220);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(218, 54);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // accountCreate
+            // 
+            this.accountCreate.Location = new System.Drawing.Point(267, 312);
+            this.accountCreate.Name = "accountCreate";
+            this.accountCreate.Size = new System.Drawing.Size(218, 48);
+            this.accountCreate.TabIndex = 1;
+            this.accountCreate.Text = "Create Account";
+            this.accountCreate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.accountCreate);
+            this.Controls.Add(this.Login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button accountCreate;
+    }
+}
+
---
+++ AppGui/AppGui/Models/CustomerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AppGui.Models
+{
+	public class CustomerModel
+	{
+		public int passengerID { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+		public string Email { get; set; }
+		public string password { get; set; }
+	}
+}
