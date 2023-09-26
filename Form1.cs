9196a4548a611936e207b0ae4623a78f30e01333
gmmclewee <99211963+gmmclewee@users.noreply.github.com>
2023-09-26 11:58:41 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,34 @@ namespace WinFormsTest
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            string connectionString = "Server=localhost;Database=train_station;User=root;Password=12345;";
+            string query = "SELECT * FROM employees;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            // try to fill grid
+            try
+            {
+                connection.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+
+
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -47,28 +75,6 @@ namespace WinFormsTest
                 }
 
                 reader.Close();
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
-
-            // try to fill grid
-            try
-            {
-                connection.Open();
-                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
-                System.Data.DataTable dataTable = new System.Data.DataTable();
-                adapter.Fill(dataTable);
-
-                dataGridView1.DataSource = dataTable;
-
-                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
-
-
-
                 connection.Close();
             }
             catch (Exception ex)
