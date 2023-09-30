8a97a4c802334c00e398450d5c583f1ae162d9f1
Quang Ho <mxrshmalloww@gmail.com>
2023-09-30 09:17:02 -0400

    testing

--- AppGui/AppGui/LoginMenu.cs
+++ AppGui/AppGui/LoginMenu.cs
@@ -12,6 +12,10 @@ namespace AppGui
 {
     public partial class LoginMenu : Form
     {
+        public static string email;
+        public static string password;
+        private string defaultEmailBox = "Email";
+        private string defaultPassBox = "Password";
         public LoginMenu()
         {
             InitializeComponent();
