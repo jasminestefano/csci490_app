a96609d86d8fd8b3a66dd2ff37d073373b49a9ec
Quang Ho <mxrshmalloww@gmail.com>
2023-10-15 09:11:26 -0400

    finished up account creation, will test later

--- AppGui/AppGui/CreateMenu.Designer.cs
+++ AppGui/AppGui/CreateMenu.Designer.cs
@@ -36,6 +36,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.SubmitBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // EnterFirstName
@@ -44,6 +45,7 @@
             this.EnterFirstName.Name = "EnterFirstName";
             this.EnterFirstName.Size = new System.Drawing.Size(100, 20);
             this.EnterFirstName.TabIndex = 0;
+            this.EnterFirstName.TextChanged += new System.EventHandler(this.EnterFirstName_TextChanged);
             // 
             // EnterLastName
             // 
@@ -103,11 +105,22 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Password";
             // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Location = new System.Drawing.Point(292, 281);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(98, 29);
+            this.SubmitBtn.TabIndex = 8;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
             // CreateMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SubmitBtn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -133,5 +146,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SubmitBtn;
     }
 }
--- AppGui/AppGui/CreateMenu.cs
+++ AppGui/AppGui/CreateMenu.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using AppGui.Services;
 namespace AppGui
 {
     public partial class CreateMenu : Form
@@ -16,6 +16,9 @@ namespace AppGui
         public static string lastName;
         public static string email;
         public static string password;
+        CustomerServices customerServices = new CustomerServices();
+
+        
         public CreateMenu()
         {
             InitializeComponent();
@@ -25,6 +28,28 @@ namespace AppGui
         {
 
         }
-        
+
+        private void EnterFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            firstName= EnterFirstName.Text;
+            lastName= EnterLastName.Text;
+            email= EnterEmail.Text;
+            password= EnterPassword.Text;
+            bool validateFirstName = customerServices.validateFirstName(firstName);
+            bool validateLastName = customerServices.validateLastName(lastName);
+            bool validateEmail = customerServices.validateEmail(email);
+            bool validatePassword = customerServices.validatePassword(password);
+            if(validateFirstName && validateLastName && validateEmail && validatePassword) {
+                customerServices.registerAccount(firstName, lastName,email,password);
+                this.Hide();
+                Login l = new Login();
+                l.Show();
+            }
+        }
     }
 }
